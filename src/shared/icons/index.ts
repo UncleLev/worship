f96ffe55adffbf7ad52860f26f16f6908c338e4a
UncleLev <risentean@gmail.com>
2023-11-02 00:40:24 +0200

    [rand] add rand song filter

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -3,3 +3,5 @@ export { default as RightArrowIcon } from "./right-arrow-icon";
 export { default as LeftArrowIcon } from "./left-arrow-icon";
 export { default as ShareIcon } from "./share-icon";
 export { default as ChordIcon } from "./chords-icon";
+export { default as RandIcon } from "./rand-icon";
+export { default as ResetIcon } from "./rest-icon";
