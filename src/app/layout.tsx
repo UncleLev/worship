9bf7862dbb21f1dcd8d13e0660667bb2c15c9c51
UncleLev <risentean@gmail.com>
2024-03-11 10:48:51 +0200

    [fix] fix theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "@/shared/styles/global.scss";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import Head from "next/head";
-// import { Meta } from "./meta";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -13,9 +11,6 @@ export const metadata: Metadata = {
     },
     description: "Міні пісеник",
     icons: "./favicon-32x32.png",
-    // referrer: {
-    // },
-    themeColor: "#009688",
 };
 
 export default function RootLayout({
@@ -25,21 +20,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="ua">
-            {/* <Head>
-                <title>sdfsdfsf</title>
-                <meta name="theme-color" content="#009688" />
-                <meta content="#009688" name="theme-color" />
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta
-                    name="apple-mobile-web-app-status-bar-style"
-                    content="black or black-translucent"
-                />
-                <meta content="yes" name="apple-mobile-web-app-capable" />
-                <meta
-                    content="black-translucent"
-                    name="apple-mobile-web-app-status-bar-style"
-                />
-            </Head> */}
             <body className={inter.className}>{children}</body>
         </html>
     );
